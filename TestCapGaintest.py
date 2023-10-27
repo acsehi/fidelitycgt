d993d4db9af0df9c5e779c95fb4b315106bb0a37
Andras Csehi <andras.csehi@outlook.com>
2023-10-27 20:51:22 +0100

    fix

---
+++ TestCapGaintest.py
@@ -0,0 +1,21 @@
+import unittest
+from capgainCalculator import run
+
+
+class TestCapGain(unittest.TestCase):
+    def test_exchangeHMRC(self):
+        run()
+        lines = self.readFileLines('cgt.tsv')
+
+        self.assertEqual(lines[0], 'B\t31/08/2023\tMSFT\t10\t150.0\t0\t0\n')
+        self.assertEqual(lines[1], 'B\t28/02/2019\tMSFT\t10.0\t200.0\t0\t0\n')
+        self.assertEqual(lines[2], 'S\t04/04/2022\tMSFT\t10.0\t150.0\t0\t0\n')
+        self.assertEqual(len(lines), 3)
+
+    def readFileLines(self, filename):
+        with open(filename) as f:
+            return f.readlines()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ capgainCalculator.py
@@ -0,0 +1,155 @@
+# http://cgtcalculator.com/
+import csv
+import datetime
+import urllib.request
+import json
+import xml.etree.ElementTree as ET
+
+
+trial_base64 = "c36cefa5b34520b268302b35c738e5ba"
+
+use_exchange = True
+use_HMRC_exchange_rates = True
+exchange_rate_cache = {}
+
+
+def convert_to_gbp(date):
+    if (not (use_exchange)):
+        return 1.0
+    if (use_HMRC_exchange_rates):
+        try:
+            return exchange_HMRC(date.strftime('%m%y'))
+        except Exception as ex:
+            if (ex.code == 404):
+                print(
+                    'HMRC don\'t have data before 2015-01-01, reverting back to daily exchange rates')
+                return exchange(date.strftime('%Y-%m-%d'))
+            else:
+                raise ex
+    else:
+        return exchange(date.strftime('%Y-%m-%d'))
+
+
+def parse_xml(xml):
+    """Parse XML file into a list of lists"""
+    tree = ET.fromstring(xml)
+    for row in tree.findall('exchangeRate'):
+        if (row.find('currencyCode').text == 'USD'):
+            return row.find('rateNew').text
+
+
+def exchange(date):
+    if (date in exchange_rate_cache):
+        print("Using cached exchange rate for " + date)
+        return exchange_rate_cache[date]
+    url = 'http://api.exchangerate.host/convert?access_key=' + \
+        trial_base64+'&from=USD&to=GBP&amount=1&date=' + date
+    with urllib.request.urlopen(url) as f:
+        r = json.loads(f.read().decode('utf-8'))
+        exchange_rate = r["result"]
+        exchange_rate_cache[date] = exchange_rate
+        print(url + " downloaded, exchange rate: " + str(exchange_rate))
+
+        return exchange_rate
+
+
+def exchange_HMRC(date):
+    if (date in exchange_rate_cache):
+        print("Using cached exchange rate for " + date)
+        return exchange_rate_cache[date]
+
+    url = 'https://www.hmrc.gov.uk/softwaredevelopers/rates/exrates-monthly-'+date+'.XML'
+    with urllib.request.urlopen(url) as f:
+        exchange_rate = 1.0/float(parse_xml(f.read().decode('utf-8')))
+        exchange_rate_cache[date] = exchange_rate
+        print(url + " downloaded, exchange rate: " + str(exchange_rate))
+        return exchange_rate
+
+
+def save_exchange_rates(exchange_rate_cache):
+    with open('exchange_rate_cache.json', 'w') as fp:
+        json.dump(exchange_rate_cache, fp)
+
+
+def load_exchange_rates():
+    with open('exchange_rate_cache.json') as json_file:
+        return json.load(json_file)
+
+
+def validate_currency(row):
+    if (row[0] == "The values are displayed in GBP"):
+        raise Exception("Please download history in USD")
+
+
+date_open = []
+quantity_open = []
+value_open = []
+exchange_rate_cache = load_exchange_rates()
+
+
+def run():
+    with open('View open lots.csv') as open_csv:
+        reader = csv.reader(open_csv, delimiter=',')
+        for row in reader:
+            if (len(row) > 2 and row[1] != "Quantity"):
+                d = datetime.datetime.strptime(
+                    row[0], '%b-%d-%Y').strftime('%d/%m/%Y')
+                exchange_rate = convert_to_gbp(
+                    datetime.datetime.strptime(row[0], '%b-%d-%Y'))
+                date_open.append(d)
+                quantity_open.append(row[1])
+                value_open.append(float(row[3])*float(exchange_rate))
+            else:
+                validate_currency(row)
+
+    date_close = []
+    quantity_close = []
+    cost_close = []
+    with open('View closed lots.csv') as open_csv:
+        # Row schema: Date acquired,Quantity,Date sold or transferred,Proceeds,Cost basis,Gain/loss,Term
+        reader = csv.reader(open_csv, delimiter=',')
+        for row in reader:
+            if (len(row) > 2 and row[1] != "Quantity"):
+                # buy
+                quantity = float(row[1])
+                buy_date = row[0]
+                do = datetime.datetime.strptime(
+                    buy_date, '%b/%d/%Y').strftime('%d/%m/%Y')
+
+                exchange_rate_buy = convert_to_gbp(
+                    datetime.datetime.strptime(buy_date, '%b/%d/%Y'))
+
+                date_open.append(do)
+                quantity_open.append(quantity)
+                cost_basis = row[4]
+                value_open.append(
+                    (float(cost_basis)/float(quantity))*float(exchange_rate_buy))
+
+                # sell
+                sell_date = row[2]
+                dc = datetime.datetime.strptime(
+                    sell_date, '%b/%d/%Y').strftime('%d/%m/%Y')
+                exchange_rate_sell = convert_to_gbp(
+                    datetime.datetime.strptime(sell_date, '%b/%d/%Y'))
+
+                date_close.append(dc)
+                quantity_close.append(quantity)
+                proceeds = row[3]
+                cost = float(proceeds)/float(quantity)
+                cost_close.append(cost*float(exchange_rate_sell))
+            else:
+                validate_currency(row)
+
+    with open('cgt.tsv', 'w', newline='',) as csvfile:
+        fieldnames = ['Action', 'Date', 'Stock_Name',
+                      'Quantity', 'price', 'brokercost', 'tax']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='\t')
+        for t in range(0, len(date_open)):
+            writer.writerow({'Action': 'B', 'Date': date_open[t], 'Stock_Name': 'MSFT',
+                            'Quantity': quantity_open[t], 'price': value_open[t], 'brokercost': '0', 'tax': '0'})
+
+        for c in range(0, len(date_close)):
+            writer.writerow({'Action': 'S', 'Date': date_close[c], 'Stock_Name': 'MSFT',
+                            'Quantity': quantity_close[c], 'price': cost_close[c], 'brokercost': '0', 'tax': '0'})
+
+    save_exchange_rates(exchange_rate_cache)
